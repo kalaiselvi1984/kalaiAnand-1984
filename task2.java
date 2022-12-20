eb71406971704ae639f0e4a60819736d6d0c7f1a
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-20 11:23:52 +0530

    Task2(datatype).

---
+++ task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args)
+    {
+        boolean user = false;
+
+        if ( user == true) {
+            System.out.println("it's true");
+        }
+        else {
+            System.out.println("it's false");
+        }
+
+
+
+    }
+}
