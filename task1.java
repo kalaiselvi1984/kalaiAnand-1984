3dc71eac8bca79169498544574a3945327725f05
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-20 11:22:55 +0530

    Data type(task1).

---
+++ task1.java
@@ -0,0 +1,13 @@
+public class task1 {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        double b = 20.3;
+        double c = 3.14785;
+
+        System.out.println("The value of a is : "+ a);
+        System.out.println("The value of b is : "+ b);
+        System.out.println("The value of c is : "+ c);
+    }
+
+}
