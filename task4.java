4f244ffd89539105ef31973233963b873904b569
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-20 11:24:57 +0530

    Task4(data type).

---
+++ task4.java
@@ -0,0 +1,16 @@
+public class task4 {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+
+
+        // convert int to string
+        // using toString()
+        String str1 = Integer.toString(num1);
+
+        // print string variables
+        System.out.println(str1 + 3);    // 5
+
+    }
+}
+
