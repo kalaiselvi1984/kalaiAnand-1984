d273351901ee54a2edb694564f45a2a73ae5b0cd
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-20 11:25:39 +0530

    Task5(Data type).

---
+++ task5.java
@@ -0,0 +1,16 @@
+public class task5 {
+    public static void main(String[] args) {
+
+        String str = "5";
+
+            Integer  number = Integer.valueOf(str);
+            System.out.println("number: "+number + 5); // output = 30
+        }
+
+        }
+
+
+
+
+
+
