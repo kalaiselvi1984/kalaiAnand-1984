8d0932bb04d770e6e32ce62b1f14b0706a063684
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-23 14:50:07 +0530

    session2 task1

---
+++ s2task1.java
@@ -0,0 +1,45 @@
+
+import java.util.*;
+public class s2task1
+{
+
+                public static void main(String[] args)
+                {
+                    Scanner sc= new Scanner(System.in);  //System.in is a standard input stream
+                    System.out.print("Enter a string: ");
+                    String str= sc.next();   //reads string before the space
+                    System.out.print("You have entered: "+str);
+                }
+            }
+
+
+
+
+
+/*{
+    public static void main(String args[])
+
+    {
+
+        int num=123456789;
+
+        int reversenum =0;
+
+        while( num != 0 )
+
+        {
+
+            reversenum = reversenum * 10;
+
+            reversenum = reversenum + num%10;
+
+            num = num/10;
+
+        }
+
+        System.out.println("Reverse of specified number is: "+reversenum);
+
+    }
+
+}*/
+
