6f0b82be42eadadbf92978c6ea63aa2ff21a773e
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-29 00:53:55 +0530

    session2 task3

---
+++ s2task3.java
@@ -0,0 +1,12 @@
+public class s2task3 {
+    public static void main(String[] args) {
+
+        // create a string
+        String eg = "Refrigerator";
+        System.out.println("String: " + eg);
+
+        // get the length of greet
+        int length = eg.length();
+        System.out.println("Length: " + length);
+    }
+}
