d05cbe9a298e044f158e72666863991fe41bc2c1
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2022-12-28 12:48:23 +0530

    session 2 task2

---
+++ s2task2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class s2task2 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String str = in.nextLine();
+    }
+}
+
+/*        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            char ch = str.charAt(i);
+            if (ch == ' ')
+                wCount++;
+            else
+                lCount++;*/
+
+
+        /*
+         * Number of words in a sentence are one more than
+         * the number of spaces so incrementing wCount by 1
+
+        wCount++;*/
+
