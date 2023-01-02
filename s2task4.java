0d8af7a0256dd89f1e539e392252d3e0935f3d0c
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2023-01-02 15:29:18 +0530

    s2task4

---
+++ s2task4.java
@@ -0,0 +1,14 @@
+class s2task4{
+    public static void main(String[] args){
+        String a = "Umbrella";
+        boolean per = false;
+        for(int i = 0;i<a.length();i++){
+            if(a.charAt(i) == 'e'){
+                per=true;
+                break;
+            }
+        }
+        System.out.println(per);
+    }
+}
+
