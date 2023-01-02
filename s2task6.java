7912974f3fc9ede476170b85442e7ba6f45ea52d
kalaiselvi1984 <120711959+kalaiselvi1984@users.noreply.github.com>
2023-01-02 17:53:09 +0530

    session2 task6

---
+++ s2task6.java
@@ -0,0 +1,17 @@
+public class s2task6 {
+    public static void main(String[] args) {
+        String s = "Hello, World";
+
+        // Find the first occurrence of 'o'
+        int first_o = s.indexOf('o');
+
+
+        // Find the last occurrence of 'o'
+        int last_o = s.lastIndexOf('o');
+
+
+        System.out.println("First occurrence of 'o': " + first_o);
+        System.out.println("Last occurrence of 'o': " + last_o);
+
+    }
+}
